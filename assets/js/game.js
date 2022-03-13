b532862913bac1518567943fe48de1b0560b34c7
Derricka S <derrickas728@gmail.com>
2022-03-13 18:42:11 -0400

    add combatants using arrays

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,3 +1,9 @@
+// Game States
+// "WIN" - Player robot has defeated all enemy-robots
+//      * Fight all enemy-robots
+//      * Defeat each enemy-robot
+// "LOSE" - Player robot's health is zero or less  
+
 var playerName = window.prompt("What is your robot's name?");
 var playerHealth = 100;
 var playerAttack = 10;
@@ -5,9 +11,14 @@ var playerMoney = 10;
 
 console.log(playerName, playerAttack, playerHealth);
 
-var enemyName = "Roborto";
+var enemyNames = ["Roborto", "Amy Android", "Robo Trumble"];
 var enemyHealth = 50;
 var enemyAttack = 12;
+
+console.log(enemyNames[0]);
+console.log(enemyNames[1]);
+console.log(enemyNames[2]);
+console.log(enemyNames.length);
 
 var fight = function() {
     
@@ -66,4 +77,4 @@ var fight = function() {
     }
 }
 
-fight();
+//fight();
